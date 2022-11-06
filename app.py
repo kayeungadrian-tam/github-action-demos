e4aac0ca6464126260098f85671d1e681ac2f248
adrian <tamkayeung.adrian@gmail.com>
2022-11-06 22:11:57 +0900

    rollback

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from data import CLASS
 
 app = FastAPI()
 
 
 @app.get("/", tags=["Root"])
-async def read_root() -> dict:
-    return {
-        "message": "Welcome to my notes application, use the /docs route to proceed"
-    }
+def read_root():
+    return {"message": "Server is up and running!"}
+
+
+@app.get("/users")
+def post_message(name: str):
+    return {"message": f"Hello {name} from {CLASS}"}
