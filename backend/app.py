a43a43f33a7f30a9e98e85c372d72ed646c6ab8c
adrian <tamkayeung.adrian@gmail.com>
2022-11-06 21:41:30 +0900

    update

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+
+
+from .routes import router
 from backend.data import CLASS
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Server is up and running!"}
+@app.get("/", tags=["Root"])
+async def read_root() -> dict:
+    return {"message": "Welcome!"}
 
 
 @app.get("/users")
 def post_message(name: str):
     return {"message": f"Hello {name} from {CLASS}"}
+
+
+app.include_router(router, prefix="/note")
