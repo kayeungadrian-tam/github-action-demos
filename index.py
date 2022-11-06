f749f02d97517c0ee2170a551eb1b1be626de9b9
adrian <tamkayeung.adrian@gmail.com>
2022-11-07 00:01:23 +0900

    update

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from data import CLASS
 
 app = Flask(__name__)
@@ -9,6 +10,23 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.get("/users")
-def get_users(name: str):
-    return f"<p>Hello, {name}!</p>"
+@app.route("/users/<name>", methods=["GET", "POST", "DELETE"])
+def user(name: str):
+
+    if request.method == "GET":
+        return f"<p>Get! Name: {name}</p>"
+
+
+@app.route("/docs", methods=["GET", "POST"])
+def get_params():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    print(email)
+    print(password)
+
+    args = request.args
+    name = args.get("name", default="", type=str)
+    args.get("price", default=0, type=int)
+    print(name)
+    return "<p>Docs!</p>"
